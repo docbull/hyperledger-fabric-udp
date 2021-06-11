8b9a045e7ad98721b13d97bdba99a2367914acd1
docbull <fudghks21@gmail.com>
2021-06-11 17:06:50 +0900

    DES module has added

--- inlab-udp/udp.go
+++ inlab-udp/udp.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
+	"crypto/cipher"
+	"crypto/des"
 	"encoding/json"
 	"fmt"
 	fountain "gofountain"
 	"log"
 	"math"
 	"net"
+	"runtime"
 	"strings"
+	"time"
 
 	udp "github.com/docbull/inlab-fabric-udp-proto"
 	protoG "github.com/golang/protobuf/proto"
@@ -22,6 +27,8 @@ import (
 type Message struct {
 	Block           *udp.Envelope
 	PeerContainerIP string
+	Key             []byte
+	IV              []byte
 }
 
 type AttrMsg struct {
@@ -35,6 +42,58 @@ type UDPEnvelope struct {
 
 var UDPenvelope UDPEnvelope
 var attr AttrMsg
+
+var Count int
+
+var timeSum time.Duration
+var desDecTime time.Duration
+
+// -------------------------- DES Cryptography -------------------------------
+
+func DesEncryption(key, iv, plainText []byte) ([]byte, error) {
+
+	block, err := des.NewCipher(key)
+
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	origData := PKCS5Padding(plainText, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, iv)
+	cryted := make([]byte, len(origData))
+	blockMode.CryptBlocks(cryted, origData)
+	return cryted, nil
+}
+
+func DesDecryption(key, iv, cipherText []byte) ([]byte, error) {
+
+	block, err := des.NewCipher(key)
+
+	if err != nil {
+		return nil, err
+	}
+
+	blockMode := cipher.NewCBCDecrypter(block, iv)
+	origData := make([]byte, len(cipherText))
+	blockMode.CryptBlocks(origData, cipherText)
+	origData = PKCS5UnPadding(origData)
+	return origData, nil
+}
+
+func PKCS5Padding(src []byte, blockSize int) []byte {
+	padding := blockSize - len(src)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+func PKCS5UnPadding(src []byte) []byte {
+	length := len(src)
+	unpadding := int(src[length-1])
+	return src[:(length - unpadding)]
+}
+
+// --------------------------------------------------------------------------
 
 func (msg *Message) WaitPeerConnection() {
 	conn, err := net.Listen("tcp", ":20000")
@@ -125,16 +184,17 @@ func (msg *Message) SendResponse(conn *net.UDPConn, addr *net.UDPAddr, res strin
 }
 
 func (msg *Message) handleUDPConnection(serv *net.UDPConn) {
-	// RT decoding, 64 symbols and 5 extra symbols
-	codec := fountain.NewRaptorCodec(128, 7)
-	dec := codec.NewDecoder(128)
+	// RT decoding, 128 symbols
+	codec := fountain.NewRaptorCodec(128, 4)
+	dec := codec.NewDecoder(128 * 4)
 	var encSymbols []fountain.LTBlock
 
 	// envelope for block unmarshalling
 	// buf for receiving RT symbols of block
-	buf := make([]byte, 1024*10)
+	buf := make([]byte, 4096*10)
 
 	n, remoteaddr, err := serv.ReadFromUDP(buf)
+	fmt.Println("Message RECEIVE", time.Now())
 	if err != nil {
 		fmt.Printf("Some error %v", err)
 		return
@@ -145,6 +205,8 @@ func (msg *Message) handleUDPConnection(serv *net.UDPConn) {
 	log.Println("Received size of the Block data:", n)
 
 	if n < 30 {
+		Count = 0
+
 		// receiving block size
 		err := json.Unmarshal(buf, &attr)
 		if err != nil {
@@ -153,24 +215,44 @@ func (msg *Message) handleUDPConnection(serv *net.UDPConn) {
 
 		fmt.Println("Received Block Size:", attr.Size)
 	} else {
+		startDES := time.Now()
+		desMsg, _ := DesDecryption(msg.Key, msg.IV, buf)
+		endDES := time.Since(startDES)
+		desDecTime += endDES
+		fmt.Println("DES Decryption delay:", endDES)
+
+		fmt.Println("DES decoded data size:", len(desMsg))
 		// receiving encoding symbol slices
-		err = json.Unmarshal(buf, &encSymbols)
+		err = json.Unmarshal(desMsg, &encSymbols)
 		if err != nil {
 			panic(err)
 		}
 
 		// if success to docode, return the original symbols
+		startTime := time.Now()
 		errCheck := decoder(encSymbols, codec, dec)
+		elapsed := time.Since(startTime)
+		timeSum += elapsed
+		fmt.Println("Elased Time for Decodeing:", elapsed)
+
+		Count += int(elapsed)
+
 		if errCheck != nil {
 			fmt.Println("Complete recovery!")
+
 			attr.Cnt += len(errCheck)
-			fmt.Println("attr.Cnt:", attr.Cnt)
+			fmt.Println("Count:", attr.Cnt)
 
 			res := "received"
 			go msg.SendResponse(serv, remoteaddr, res)
 
 			UDPenvelope.Envelope = append(UDPenvelope.Envelope, errCheck[:len(errCheck)]...)
-			if attr.Cnt >= attr.Size {
+			fmt.Println("length of UDP Envelope:", len(UDPenvelope.Envelope))
+			if len(UDPenvelope.Envelope) >= attr.Size {
+				fmt.Println("RT Decoding time:", timeSum)
+				timeSum = 0
+				fmt.Println("DES Decoding time:", desDecTime)
+				desDecTime = 0
 
 				fmt.Println("************")
 				fmt.Println("END OF BLOCK")
@@ -198,7 +280,6 @@ func (msg *Message) UDPServerListen() {
 		msg.handleUDPConnection(serv)
 	}
 }
-
 func (msg *Message) UDPBlockSender() {
 	conn, err := net.Dial("udp", "192.168.1.2:8000")
 	if err != nil {
@@ -208,7 +289,7 @@ func (msg *Message) UDPBlockSender() {
 	defer conn.Close()
 
 	symbols := 128
-	extraSymbols := 7
+	redundancySymbols := 7
 
 	envelope, err := protoG.Marshal(msg.Block)
 	if err != nil {
@@ -218,7 +299,7 @@ func (msg *Message) UDPBlockSender() {
 	fmt.Println("Marshalled data size:", len(envelope))
 
 	// Raptor encoding
-	codec := fountain.NewRaptorCodec(symbols, extraSymbols)
+	codec := fountain.NewRaptorCodec(symbols, 4)
 
 	// sent block size first
 	attrMsg := AttrMsg{Size: len(envelope), Cnt: 0}
@@ -231,41 +312,64 @@ func (msg *Message) UDPBlockSender() {
 		log.Println(err)
 	}
 
-	iter := int(math.Ceil(float64(len(envelope)) / float64(symbols)))
+	iter := int(math.Ceil(float64(len(envelope)) / (float64(symbols) * 4)))
+	fmt.Println("iter:", iter)
 
 	var start, end int
 	var ltBlks []fountain.LTBlock
 
-	index := make([]int64, symbols+extraSymbols)
-	for i := 0; i < symbols+extraSymbols; i++ {
+	index := make([]int64, (symbols*4)+redundancySymbols)
+	for i := 0; i < (symbols*4)+redundancySymbols; i++ {
 		index[i] = int64(i)
 	}
 
 	sum := 0
+
+	var encodingTime time.Duration
+	var desTime time.Duration
+
+	startTime := time.Now()
 	for i := 0; i < iter; i++ {
-		if (sum + symbols) > len(envelope) {
+		if (sum + (symbols * 4)) > len(envelope) {
 			start = end
 			end = len(envelope)
 			sum += (end - start)
 		} else {
-			start = (symbols * i)
-			end = (symbols * (i + 1))
-			sum += symbols
+			start = ((symbols * 4) * i)
+			end = ((symbols * 4) * (i + 1))
+			sum += (symbols * 4)
 		}
+		fmt.Println("start:", start)
+		fmt.Println("end:", end)
 		slice := envelope[start:end]
+		fmt.Println("size of slice:", len(slice))
 
+		startEncoding := time.Now()
 		ltBlks = fountain.EncodeLTBlocks(slice, index, codec)
-		ltBlks = append(ltBlks[:46], ltBlks[50:134]...)
+		endEncoding := time.Since(startEncoding)
+		fmt.Println("Elapsed Time for Encoding:", endEncoding)
+		encodingTime += endEncoding
+		fmt.Println("Size of RT Block:", len(ltBlks))
+		//ltBlks = append(ltBlks[:46], ltBlks[50:519]...)
 
 		message, err := json.Marshal(ltBlks)
 		if err != nil {
 			log.Fatal(err)
 		}
-		_, err = conn.Write(message)
+		fmt.Println("length of marshalled msg:", len(message))
+
+		startDES := time.Now()
+		desMsg, _ := DesEncryption(msg.Key, msg.IV, message)
+		endDES := time.Since(startDES)
+		desTime += endDES
+		fmt.Println("DES Decryption delay:", endDES)
+
+		fmt.Println("Message SEND", time.Now())
+		n, err := conn.Write(desMsg)
 		if err != nil {
 			log.Println(err)
 		}
-		fmt.Println("Wrote data size:", sum)
+		fmt.Println("Wrote data size:", n)
 
 		p := make([]byte, 1024)
 		_, err = bufio.NewReader(conn).Read(p)
@@ -273,11 +377,19 @@ func (msg *Message) UDPBlockSender() {
 			fmt.Printf("Some error %v\n", err)
 		}
 		resMsg := string(p)
-		log.Println("Received size of the block:", resMsg)
+		log.Println("Reponse message from UDP:", resMsg)
+		fmt.Println()
 	}
+	fmt.Println()
+	elapsed := time.Since(startTime)
+	fmt.Println("RTUDP transmission latency:", elapsed)
+	fmt.Println("RT Encoding latency:", encodingTime)
+	fmt.Println("DES Encoding latency:", desTime)
+	fmt.Println("------------------------------------------")
 }
 
 func (msg *Message) BlockDataForUDP(ctx context.Context, envelope *udp.Envelope) (*udp.Status, error) {
+	fmt.Println("------------------------------------------")
 	log.Println("Receive Block data from the Peer container")
 
 	msg.Block.Payload = envelope.Payload
@@ -317,6 +429,8 @@ func start() {
 	msg := Message{
 		Block:           nil,
 		PeerContainerIP: "",
+		Key:             []byte{0xBC, 0xBC, 0xBC, 0xBC, 0xBC, 0xBC, 0xBC, 0xBC},
+		IV:              []byte{0xBC, 0xBC, 0xBC, 0xBC, 0xBC, 0xBC, 0xBC, 0xBC},
 	}
 	msg.Block = &udp.Envelope{
 		Payload:        nil,
@@ -338,5 +452,8 @@ func start() {
 }
 
 func main() {
+	runtime.GOMAXPROCS((runtime.NumCPU()))
+	fmt.Println("CPU:", runtime.GOMAXPROCS(0))
+
 	start()
 }
